72586fdc1714e6df385055aeb9e25ba1bc5f013e
Takeshi Kubo <e225736@ie.u-ryukyu.ac.jp>
2022-12-13 06:04:19 +0900

    step2

--- src/Report5.java
+++ src/Report5.java
@@ -1,6 +1,14 @@
+import java.lang.NullPointerException;
+
 public class Report5{
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try{
+            str = null;
+            System.out.println(str.length());
+        } catch(NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
